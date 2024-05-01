73262918446dba0e3a3426cf9f122c3c26d48678
Adam <136097061+Adam12214746@users.noreply.github.com>
2024-05-01 22:46:24 +0900

    Update serializers.py

--- innovateXapi/api/serializers.py
+++ innovateXapi/api/serializers.py
@@ -3,3 +3,5 @@ from rest_framework import serializers
 class ChatSerializer(serializers.Serializer):
     message = serializers.CharField(max_length=2000)
 
+class TextSerializer(serializers.Serializer):
+    text = serializers.CharField(max_length=10000) 
