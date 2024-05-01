406cdbbea64b3c3a66ab4dc66208b98f5871f02f
Aziz <ahmadmuminov16@gmail.com>
2024-05-01 22:07:31 +0900

    added paraphrase api with api tests

--- innovateXapi/api/urls.py
+++ innovateXapi/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ChatView
+from .views import ChatView, ParaphraseView
 
 urlpatterns = [
     path('chat/', ChatView.as_view(), name='chat'),
+    path('paraphrase/', ParaphraseView.as_view(), name='paraphrase'),
 ]
